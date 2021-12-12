7735be68066b932e2e88c82a48f37ecc4de6b60c
widadanncoud <71724347+widadanncoud@users.noreply.github.com>
2021-12-12 22:57:28 +0100

    changes

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const bodyParser =  require ('body-parser')
 const app = express()
 
 //definiëren welke port the backend mag openen om te kunnen gebruiken
-const port = process.env.PORT || 3000
-const username = "team_amina"
-const password = "thisisourpassword"
+const port = process.env.PORT || 4000
+const username = "admin"
+const password = "admin"
 
 //folder where we get data is going to be called public
 app.use(express.static('public')) 
@@ -37,6 +37,29 @@ app.get('/', (req, res)=> {
 app.get('/mct', (req, res)=> {
     res.send("hall");
 })
+
+//Return all challenges from database
+app.get('/challenges', async (req, res) => {
+  try {
+    //connect to the database
+    await client.connect();
+    const db = client.db("web2CP");
+    // Use the searchCriteria "web2CP"
+    const collection = db.collection("searchCriteria");
+    // Find document
+    const data = await collection.find({}).toArray();
+
+    // Print to the console
+    console.log(data);
+
+    res.status(200).send(data)
+  } catch (err) { //catch an error
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+});
+
 app.post('/test', (req, res)=> {
     const body = req.body;
     console.log(body)
